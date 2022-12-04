cdce2944dcc09dbfe9d7a5be01ac8d88966a28ec
r2r2 <r2r2@yahoo.com>
2022-12-04 13:31:28 +0300

    refactor(tests): fix tests

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -11,11 +11,10 @@ import (
 )
 
 func createRandomAccount(t *testing.T) Account {
-	//user := createRandomUser(t)
+	user := createRandomUser(t)
 
 	arg := CreateAccountParams{
-		//Owner:    user.Username,
-		Owner:    util.RandomOwner(),
+		Owner:    user.Username,
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
