fe021891f0e425453e4e359fc0300a123862dd54
r2r2 <r2r2@yahoo.com>
2023-02-27 14:01:45 +0700

    feature(email): add email sending

---
+++ mail/sender_test.go
@@ -0,0 +1,29 @@
+package mail
+
+import (
+	"github.com/r2r2/simplebank/util"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+	subject := "A test email"
+	content := `
+	<h1>Hello world</h1>
+	<p>This is a test message from <a href="https://medium.com">Im learning go</a></p>
+	`
+	to := []string{"learninggo@gmail.com"}
+	attachFiles := []string{"../README.md"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
